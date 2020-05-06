e5ae654f6e458e61e09d70eff7cd7a3c0572a859
lukecool@gmail.com <lukecool@gmail.com>
2020-05-05 22:22:14 -0500

    dual stick gamepad support!

--- Objects/aim-visual.gd
+++ Objects/aim-visual.gd
@@ -2,4 +2,17 @@ extends Node2D
 
 func _process(delta):
 	var player = get_parent()
+	
+	# Mouse based aiming
 	rotation = player.get_local_mouse_position().angle()
+
+	# Right-Stick (GamePad) Based Aiming
+	# Code below from https://godotengine.org/qa/35254/how-to-aim-right-game-pad-analog-stick
+	var deadzone = 0.20
+	var controllerangle = Vector2.ZERO
+	var xAxisRL = Input.get_joy_axis(0, JOY_AXIS_2)
+	var yAxisUD = Input.get_joy_axis(0 ,JOY_AXIS_3)
+	
+	if abs(xAxisRL) > deadzone || abs(yAxisUD) > deadzone:
+	    controllerangle = Vector2(xAxisRL, yAxisUD).angle()
+	    rotation = controllerangle
