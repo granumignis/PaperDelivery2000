a9eb9f960dfad1521ac65fc93a8d9c650e088089
lukecool@gmail.com <lukecool@gmail.com>
2020-10-29 23:13:49 -0500

    houses on bottom now smoothly fade out and in if paperboy intersects them

--- Objects/House3.gd
+++ Objects/House3.gd
@@ -1,7 +1,13 @@
 extends Area2D
 
 onready var Sprite = $Sprite
+onready var AnimationPlayer = $HouseAnimationPlayer
 
+var fadeout = 0
+var fadein = 0
+
+var fullbright = Color(1,1,1,1)
+var halfalpha = Color(1,1,1,.5)
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
@@ -16,16 +22,18 @@ func _ready():
 #func _process(delta):
 #	pass
 
-
 func _on_House3_area_entered(area):	
 	print(area.name + " just intersected a house sprite")
 	print(str(area.z_index) + " <-- z index of thing that intersected")
 	print(str(self.z_index) + "<-- z index of house that got intersected.")
 	if area.name in ("DigitalMan"):
-		Sprite.modulate = Color(1,1,1,.5)
+		AnimationPlayer.play("FadeOut")
+		#fadeout = 1
+		#Sprite.modulate = Color(1,1,1,.5)
 
 
 func _on_House3_area_exited(area):
 	print(area.name + " just intersected a house sprite")
 	if area.name in ("DigitalMan"):
-		Sprite.modulate = Color(1,1,1,1)
+		AnimationPlayer.play_backwards("FadeOut")
+		#Sprite.modulate = Color(1,1,1,1)
