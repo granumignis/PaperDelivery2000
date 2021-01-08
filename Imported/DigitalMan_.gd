71b58e82c6940024751f61f500623984d42fcef4
lukecool@gmail.com <lukecool@gmail.com>
2021-01-08 13:35:18 -0600

    Yes, there is now an undocumented crouch feature that is purely visual

--- Imported/DigitalMan_.gd
+++ Imported/DigitalMan_.gd
@@ -3,19 +3,23 @@ extends Area2D
 export(int) var WALKSPEED = 50
 export(int) var RUNSPEED = 100
 var SPEED = WALKSPEED
+export(bool) var hasCrouched = false
 
 var moving = false
 onready var animationPlayer = $AnimationPlayer
 onready var sprite = $Sprite
+onready var CrouchSprite = $CrouchSprite
 
 func _process(delta):
 	moving = false
 	if Input.is_action_pressed("ui_right"):
 		move(SPEED, 0, delta)
 		sprite.flip_h = false
+		CrouchSprite.flip_h = true
 	if Input.is_action_pressed("ui_left"):
 		move(-SPEED, 0, delta)
 		sprite.flip_h = true
+		CrouchSprite.flip_h = false
 	if Input.is_action_pressed("ui_up"):
 		move(0, -SPEED, delta)
 	if Input.is_action_pressed("ui_down"):
@@ -31,8 +35,16 @@ func _process(delta):
 	if moving == true:
 		animationPlayer.play("Run")
 	else:
-		animationPlayer.play("Idle")
-	pass
+		if Input.is_action_just_pressed("crouch") && animationPlayer.current_animation != "Crouch":
+			if hasCrouched == false:
+				animationPlayer.play("Crouch")
+				yield(get_tree().create_timer(animationPlayer.current_animation_length), "timeout")
+		if Input.is_action_just_pressed("crouch"):
+			if hasCrouched == true:
+				animationPlayer.play_backwards("Crouch")
+				yield(get_tree().create_timer(animationPlayer.current_animation_length), "timeout")
+			
+
 
 func move(xspeed, yspeed, delta):
 	#position.x += xspeed * delta
