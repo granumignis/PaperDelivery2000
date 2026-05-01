833f1b1c3dfa34dea1ad2984b0be542a7b18aa87
luke <luke@lukes-MBP.localdomain>
2026-05-01 08:11:33 -0500

    Copied the main level so I had a new one to noodle with.

--- Errata/Utils.gd
+++ Errata/Utils.gd
@@ -7,10 +7,8 @@ func instance_scene_on_main(scene, position):
 	instance.global_position = position
 	return instance
 
-
 func wait(delaySeconds):
 	yield(get_tree().create_timer(delaySeconds), "timeout")
-
 
 func get_by_name(nameOfThingToget):
 	return get_tree().get_root().find_node(nameOfThingToget, true, false)
